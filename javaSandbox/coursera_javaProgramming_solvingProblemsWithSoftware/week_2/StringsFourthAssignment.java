043c73ad2ede7d1579267ee2deb0cc6a96a89cf9
Ludwig <ludwig.nachname.spam@gmail.com>
2021-09-29 20:50:19 +0200

    Initial commit

---
+++ javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/StringsFourthAssignment.java
@@ -0,0 +1,25 @@
+package javaSandbox.coursera_javaProgramming_solvingProblemsWithSoftware.week_2;
+
+import edu.duke.URLResource;
+
+public class StringsFourthAssignment {
+
+    public void findLinks(String url) {
+        URLResource urlResource = new URLResource(url);
+
+        for (String word : urlResource.words()) {
+            int youtubeIndex = word.toLowerCase().indexOf("youtube.com");
+
+            if (youtubeIndex != -1) {
+                int startIndex = word.lastIndexOf("\"", youtubeIndex);
+                int lastIndex = word.indexOf("\"", youtubeIndex);
+                System.out.println("The youtube link is: " + word.substring(startIndex + 1, lastIndex));
+            }
+        }
+    }
+
+    public void test() {
+        String url = "http://www.dukelearntoprogram.com/course2/data/manylinks.html";
+        findLinks(url);
+    }
+}
