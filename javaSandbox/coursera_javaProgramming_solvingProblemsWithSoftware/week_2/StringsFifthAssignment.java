4924ed31e0b644512c16419da76b133c52296346
Ludwig <ludwig.nachname.spam@gmail.com>
2021-10-10 14:17:23 +0200

    Added finGene method

--- javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/StringsFifthAssignment.java
+++ javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/StringsFifthAssignment.java
@@ -16,6 +16,26 @@ public class StringsFifthAssignment {
         return dna.length();
     }
 
+    public String findGene(String dna) {
+        int startIndex = dna.indexOf("ATG");
+
+        if (startIndex == -1) {
+            return "";
+        }
+
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+
+        int minIndex = Math.min(taaIndex, Math.min(tagIndex, tgaIndex));
+
+        if (minIndex == dna.length()) {
+            return "";
+        }
+
+        return dna.substring(startIndex, minIndex + 3);
+    }
+
     public void testFindStopCodon() {
         String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
 
