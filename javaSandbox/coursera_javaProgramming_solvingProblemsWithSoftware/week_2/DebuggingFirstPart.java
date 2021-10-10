c37e65fa2988d8903e0ff8e8dfd009469ebf40e7
Ludwig <ludwig.nachname.spam@gmail.com>
2021-10-10 17:24:06 +0200

    Added modifications for practice exam

--- javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/DebuggingFirstPart.java
+++ javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/DebuggingFirstPart.java
@@ -2,4 +2,25 @@ package javaSandbox.coursera_javaProgramming_solvingProblemsWithSoftware.week_2;
 
 public class DebuggingFirstPart {
 
+    public void findAbc(String input) {
+        int index = input.indexOf("abc");
+        while (true) {
+            if (index == -1 || index >= input.length() - 3) {
+                break;
+            }
+            String found = input.substring(index + 1, index + 4);
+            System.out.println(found);
+            index = input.indexOf("abc", index + 4);
+        }
+    }
+
+    public void test() {
+        // findAbc("abcd");
+        findAbc("abcdabc");
+    }
+
+    public static void main(String[] args) {
+        DebuggingFirstPart dfp = new DebuggingFirstPart();
+        dfp.test();
+    }
 }
