d6631f3fd3bcbe73a66f1d784942804438307a26
Ludwig <ludwig.nachname.spam@gmail.com>
2021-10-10 17:36:31 +0200

    Modified for practice exam

--- javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/DebuggingSecondPart.java
+++ javaSandbox/coursera_javaProgramming_solvingProblemsWithSoftware/week_2/DebuggingSecondPart.java
@@ -8,15 +8,19 @@ public class DebuggingSecondPart {
             if (index == -1 || index >= input.length() - 3) {
                 break;
             }
+            System.out.println("index " + index);
             String found = input.substring(index + 1, index + 4);
             System.out.println(found);
-            index = input.indexOf("abc", index + 4);
+            index = input.indexOf("abc", index + 3);
+            System.out.println("index after updating " + index);
         }
     }
 
     public void test() {
         // findAbc("abcd");
-        findAbc("abcdabc");
+        // findAbc("abcdabc");
+        // findAbc("abcdkfjsksioehgjfhsdjfhksdfhuwabcabcajfieowj");
+        findAbc("abcabcabcabca”");
     }
 
     public static void main(String[] args) {
